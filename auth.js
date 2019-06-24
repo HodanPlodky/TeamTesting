f615a21c70f2cc69da5315cf83057141eaffac6b
HodanPlodky <a.porek@centrum.cz>
2019-06-24 15:58:23 +0200

    best auth

--- auth.js
+++ auth.js
@@ -5,73 +5,43 @@ import axios from 'axios';
 const localtoken = 'token';
 const localexpire = 'token_expire';
 
-const settings = require('../../settings.json');
+const settings = require('../settings.json');
 const tokenexp = settings.tokenexpiration;
+const isdebug = settings.debug;
 //#endregion
 
-// ! eventy a pomocné metody pro ně
-//#region Eventy
-// pomocná funkce která se spustí při lognutí
-let eventLog = () => {
-    console.log('logged')
+// handle nastavení debugu
+//#region Debug handle
+var writelog = console.log;
+var writeerr = console.error;
+
+console.log = function(log){
+    if (isdebug){
+        writelog(log);
+    }
 }
 
-// event na logout
-let eventLogOut = () => {
-    console.log('logout');
-}
-
-// funkce co se spustí při successu isAuthenticatedRefresh
-let eventRefreshSucc = () => {
-    console.log("refreshed");
-}
-
-// funkce co se spustí při failu isAuthenticatedRefresh
-let eventRefreshFail = () => {
-    console.error("epic fail");
-}
-
-let eventFail = () => {
-    console.error("epic fail");
-}
-
-// zapisování do event log
-// auth.logged([funkce co se má stát po lognutí])
-export let logged = (fce) => {
-    eventLog = fce;
-}
-
-export let loggedOut = (fce) => {
-    eventLogOut = fce;
-}
-
-// zapisování metod do eventu spojených s isAuthenticatedRefresh
-export let refreshed = (success, fail) => {
-    eventRefreshSucc = success;
-    eventRefreshFail = fail;
-}
-
-export let authError = (fce) => {
-    eventFail = fce;
-
+console.error = function(error){
+    if (isdebug){
+        writeerr(error);
+    }
 }
 //#endregion
 
 // ! zbytek metod
-// TODO : password reset, password change, start
+// TODO : password reset, password change
 //#region Funkce
 // funkce pro přihlašování uživatele
-export const logIn = (username, password, server) => {
+export const logIn = (username, password, server, success, fail) => {
         axios.post(`${server}/rest-auth/login/`, {username:username, password:password})
             .then(res => {
                 writeToken(res.data.key, settings.tokenexpiration);
-                //localStorage.setItem(localtoken, res.data.key);
                 expireCheckMin(tokenexp);
-                eventLog();
+                success();
             })
             .catch(err => {
                 console.error(err);
-                eventFail(err);
+                fail(err);
             });
 }
 
@@ -81,7 +51,9 @@ export const register = (
     email, 
     password1,
     password2,
-    server) => {
+    server,
+    success,
+    fail) => {
         axios.post(`${server}/rest-auth/registration/`,{
                 username:username,
                 email:email,
@@ -90,41 +62,38 @@ export const register = (
             })
             .then(res => {
                 localStorage.setItem(localtoken, res.data.key);
-                eventLog();
+                success();
                 expireCheckMin(tokenexp);
             })
             .catch(err => {
                 console.error(err);
-                eventFail(err);
+                fail(err);
             });
         
     
 }
 
 // nooooo..... log out
-export const logOut = (server) => {
+export const logOut = (server, success) => {
     clearToken();
-    eventLogOut();
+    success();
 }
 
 // vymaže token potom co vyprší jeho platnost
 // čas je v sekundách
 export const expireCheck = (time) => {
     console.log("expire started ",time);
-    let interval = setInterval(function(){
-        refreshed(()=>{
-            console.log("refresed");
-            writeToken(getTokenClear(), new Date().getTime() + settings.tokenexpiration * 1000 * 60);
+    isAuthenticatedRefresh(()=>{
+        console.log("refresed");
+        writeToken(getTokenClear(), new Date().getTime() + settings.tokenexpiration * 1000 * 60);
+        setTimeout(expireCheck, settings.tokenexpiration * 1000 * 60)
         },
         ()=>{
             console.log("clear");
-            clearToken();
-            clearInterval(interval);
-        })
-        isAuthenticatedRefresh()
-    }, time * 1000)
+            clearToken();});
 }
 
+// expire check ale v minutách
 export const expireCheckMin = (time) => {
     console.log("expire started min ",time);
     expireCheck(time * 60);
@@ -139,56 +108,129 @@ export const isAuthenticated = () => {
 
 // jestli je přihlášen + check i na serveru
 // 
-export const isAuthenticatedRefresh = () => {
+export const isAuthenticatedRefresh = (success, fail) => {
     console.log(settings.server_address);
     console.log("refresh started");
     if (!isAuthenticated()){
-        eventRefreshFail();
+        fail();
         return;
     }
     axios.get(`${settings.server_address}/rest-auth/user/`, {headers: { "Authorization": getToken() }})
-        .then(res => eventRefreshSucc())
-        .catch(err => eventRefreshFail());
+        .then(res => success())
+        .catch(err => fail());
 }
 
 // startnutí appky
 export const start = (server) => {
     let token = getTokenClear();
     if (token === undefined || token === null) return false;
-    refreshed(()=>{
-        const expire = getExpire();
-        const timenow = new Date().getTime();
-        if (expire <= timenow){
-            clearToken();
-        }
-        else{
-            expireCheck((expire - timenow)/1000);
-        }
-    },()=>{clearToken()});
-    isAuthenticatedRefresh(server);
+    isAuthenticatedRefresh(server, 
+        ()=>{
+            const expire = getExpire();
+            const timenow = new Date().getTime();
+            if (expire <= timenow){
+                clearToken();
+            }
+            else{
+                expireCheck((expire - timenow)/1000);
+            }
+        },
+        ()=>{clearToken()});
 
 }
 
-// dostat token aby se nemuselo líst do localStorage
+// tvar tokenu pro header
 export const getToken = () => {
     return `Token ${localStorage.getItem(localtoken)}`;
 }
 
+// dostat token aby se nemuselo líst do localStorage
 export const getTokenClear = () => {
     return localStorage.getItem(localtoken);
 }
 
+// zapsání tokenu
 export const writeToken = (key, time) => {
     localStorage.setItem(localtoken, key);
     localStorage.setItem(localexpire, new Date().getTime() + time * 1000 * 60);
 }
 
+// vymazaní tokenů
 export const clearToken = () => {
     localStorage.removeItem(localtoken);
     localStorage.removeItem(localexpire);
 }
 
+// získání expire z localstorage
 export const getExpire = () => {
     return localStorage.getItem(localexpire);
+}
+
+// loadnutí usera ze serveru
+export const getUser = (server, success, fail) => {
+    if (!isAuthenticated()) return false;
+
+    axios.get(`${server}/rest-auth/user/`, {headers: { "Authorization": getToken() }})
+        .then(res => {
+            console.log(res.data);
+            success(res.data);
+        })
+        .catch(err =>{
+            console.error(err);
+            fail(err);
+        });
+}
+
+// změna hesla
+export const changePassword = (password1, password2, server, success, fail) => {
+    axios.post(`${server}/rest-auth/password/change/`, 
+                {
+                    headers : { "Authorization": getToken() },
+                    data : {
+                        password1:password1,
+                        password2:password2
+                    }
+                })
+                .then(res => console.log(res));
+}
+
+// dotaz na server pro odeslání hesla pro reset hesla
+export const sendPasswordReset = () => {
+
+}
+
+// samotný reset
+export const resetPassword = () => {
+
+}
+
+// změna informací o userovi
+export const updateUser = (
+    pk,
+    username,
+    email,
+    password,
+    first_name,
+    last_name,
+    server,
+    success,
+    fail) => {
+    axios.put(`${server}/rest-auth/user/update/${pk}/`,
+            {
+                username:username,
+                email:email,
+                password:password,
+                first_name:first_name,
+                last_name:last_name
+            },
+            {headers : { "Authorization": getToken() }})
+            .then(res => {
+                console.log(res.data);
+                success();
+            })
+            .catch(err => {
+                console.error(err);
+                fail(err);
+            });
 }
 //#endregion
